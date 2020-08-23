aad61f6c7593f260d4d3630f58d29a91aa72d816
bridget462 <bridget.dev.create@gmail.com>
2020-08-23 13:02:37 -0700

    unsplash API added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Unsplash API
+// API Docs: https://unsplash.com/documentation#location
+const count = 10;
+const apiKey = "-S0hkdst8Ukx-auNBnAsYtemgV4EHMB-TOJrwjKJSXE";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Cath Error Here
+  }
+}
+
+// On Load
+getPhotos();
