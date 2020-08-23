c3ef3b02f06928ca19f5993df0e5386fdf1161c3
bridget462 <bridget.dev.create@gmail.com>
2020-08-23 16:28:51 -0700

    function imageLoaded added to only load nessasary images

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 // API Docs: https://unsplash.com/documentation#location
-const count = 10;
+const count = 30;
 const apiKey = "-S0hkdst8Ukx-auNBnAsYtemgV4EHMB-TOJrwjKJSXE";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper Function to Set Attribute on DOM Element
 function setAttributes(element, attributes) {
@@ -18,6 +31,9 @@ function setAttributes(element, attributes) {
 
 // Create Element For Links & Photos, Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -33,6 +49,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listner, when each is finished loading
+    img.addEventListener("load", imageLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -47,17 +65,18 @@ async function getPhotos() {
     displayPhotos();
   } catch (error) {
     // Cath Error Here
+    console.log(error);
   }
 }
 
 // Check to see if scrolling near buttom of the page, Load More Photos
 window.addEventListener("scroll", () => {
   if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight - 1000
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
   ) {
+    ready = false;
     getPhotos();
-    console.log("scroll threshold reached!");
   }
 });
 
